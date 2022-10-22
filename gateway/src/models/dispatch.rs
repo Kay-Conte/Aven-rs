b8f0e2395fd454aa8493293f90c793c6ec0ab94c
Kayy <81878924+Kay-Conte@users.noreply.github.com>
2022-10-22 18:25:56 -0400

    Begin modeling packets

---
+++ gateway/src/models/dispatch.rs
@@ -0,0 +1,4 @@
+use serde::Serialize;
+
+#[derive(Serialize, Debug, PartialEq, Eq)]
+pub struct Dispatch {}
--- gateway/src/models/mod.rs
+++ gateway/src/models/mod.rs
@@ -5,9 +5,10 @@ pub mod components;
 pub mod hello;
 pub mod identify;
 
+mod dispatch;
 mod op_codes;
 
-pub use packet::Packet;
+pub use dispatch::Dispatch;
 pub use hello::Hello;
 pub use identify::Identify;
-
+pub use packet::Packet;
